5f3ad919379b5e6e299427585fd9b74a52c2deff
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-10-20 19:18:44 +0200

    fix

---
+++ my-next-app3/.next/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_0b6d448d._.js")
+R.c("build/chunks/[root-of-the-server]__acf2e7ef._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/rekogrinikadmin/my-next-app3/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/rekogrinikadmin/my-next-app3/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
